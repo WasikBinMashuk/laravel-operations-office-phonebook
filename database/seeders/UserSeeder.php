74960da6ffef1fdf33e9ab776207978ed086d8f2
WasikBinMashuk <haquepronoy@gmail.com>
2023-08-08 18:18:31 +0600

    seeder file added

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,52 +17,41 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $numberOfUsers = 2000;
-        $users = [];
-        $phoneBook= [];
+        $numberOfUsers = 2000; 
+        // $users = [];
+        // $phoneBook= [];
         for ($i = 1; $i <= $numberOfUsers; $i++) {
-            $users[] = [
+            // users creation
+            $users = User::create([
                 'name' => 'User ' . $i,
                 'email' => 'user' . $i . '@example.com',
                 'password' => Hash::make('12345678'),
                 'created_at' => now(),
                 'updated_at' => now(),
-            ];
-
+            ]);
             
-            
-        }
+            for($j = 1; $j <= 5000; $j++){
+                // Contacts creation under every user
 
-        // User::insert($users); 
-        // $userIds = User::insertGetId($users);
+                //first 10 private contact
+                if($j <= 10){
+                    $status = 1;
+                }
+                else{
+                    $status = 0;
+                }
 
-        // Insert users into the database and get their IDs
-        $insertedUsers = User::insert($users);
-
-        // Retrieve the IDs of the inserted users
-        $userIds = $insertedUsers->pluck('id')->all();
-
-
-        for ($i = 1; $i <= $numberOfUsers; $i++){
-            $id = 0;
-            for($j = 1; $j <= 5010; $j++){
-                $phoneBook[] = [
+                PhoneBook::create([
                     'name' => 'Contact ' . $i,
                     'mobile' => mt_rand(10000000000, 99999999999),
                     'address' => 'Mirpur' . $i,
-                    'status' => rand(0,1),
-                    'ownerId' => $userIds[$id],
+                    'status' => $status,      // 10 values with 1 and others 0
+                    'ownerId' => $users -> id,
                     'created_at' => now(),
                     'updated_at' => now(),
-                    'favourite' => rand(0,1)
-                ];
+                    'favourite' => rand(0,1)    //atleast two values has to 1
+                ]);
             }
-            $id++;
         }
-
-        PhoneBook::insert($phoneBook);
-
-        
-        
     }
 }
